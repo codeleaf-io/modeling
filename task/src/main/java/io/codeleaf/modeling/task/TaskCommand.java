53098777efa22101bba95251f18f65f74b7d9ad4
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2019-01-27 00:00:06 +0100

    Adds TaskCommand

---
+++ task/src/main/java/io/codeleaf/modeling/task/TaskCommand.java
@@ -0,0 +1,32 @@
+package io.codeleaf.modeling.task;
+
+import java.util.Objects;
+
+public final class TaskCommand<O> {
+
+    private final TaskHandler taskHandler;
+    private final Task<O> task;
+
+    private TaskCommand(TaskHandler taskHandler, Task<O> task) {
+        this.taskHandler = taskHandler;
+        this.task = task;
+    }
+
+    public TaskHandler getTaskHandler() {
+        return taskHandler;
+    }
+
+    public Task<O> getTask() {
+        return task;
+    }
+
+    public O invoke() throws TaskHandlingException {
+        return taskHandler.handleTask(task);
+    }
+
+    public static <O> TaskCommand<O> create(TaskHandler taskHandler, Task<O> task) {
+        Objects.requireNonNull(taskHandler);
+        Objects.requireNonNull(task);
+        return new TaskCommand<>(taskHandler, task);
+    }
+}
