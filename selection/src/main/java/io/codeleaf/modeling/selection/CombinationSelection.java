337b5f177454f90f8d306cce10d7ae0254532e23
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2019-01-27 15:23:53 +0100

    Adds Selection type to CombinationSelection

--- selection/src/main/java/io/codeleaf/modeling/selection/CombinationSelection.java
+++ selection/src/main/java/io/codeleaf/modeling/selection/CombinationSelection.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-public interface CombinationSelection {
+public interface CombinationSelection extends Selection {
 
     abstract class AbstractBuilder<S extends CombinationSelection> {
 
